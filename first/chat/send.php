7960f6cf8de0d9150ee682064fde2e98a475001d
Yaroslav132 <42564078+Yaroslav132@users.noreply.github.com>
2018-08-23 22:42:47 +0700

    init commit

--- first/chat/send.php
+++
@@ -1,8 +0,0 @@
-<?php
-$message = $_POST[msg];
-$name = $_POST[name];
-$file = fopen("chat.html","a+");
-fputs($file,"<strong>$name</strong> <br/> $message <hr/> <br/>");
-fclose($file);
-header('location: indexx.html');
-?>
