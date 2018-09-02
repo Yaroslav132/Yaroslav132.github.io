fb7535092e6c1fd1bdee0f7bf75e2c0deff4850d
Yaroslav132 <killerkiller721@gmail.com>
2018-09-02 12:28:27 +0700

    init commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+	function hello(){alert("Hello, "+name+"!!!");
+	}
+	var name=prompt("what's your name??");
+	hello();	
+	//var mm=confirm("are you fucking serious???")//question
+		
+	for (i=1; i<=5; i++){
+		document.write(x+"_"+i+"<br/>")
+	}
+	var y=1;
+	while(y<5){
+		document.write(y+"<br/>")
+		y=y+1
+	}
+	var x=0;
+	
+	while(x<6){x++}document.write(x);
+	
