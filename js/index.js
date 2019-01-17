4b6c1cafa133636cede6d71d5c0d78d4d4f1dcab
Yaroslav132 <42564078+Yaroslav132@users.noreply.github.com>
2019-01-17 11:06:41 +0700

    init commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
-	function hello(){alert("Hello, "+name+"!!!");
-	}
+	function hell(){alert("FUCK YOU");}
+	function hello(){alert("Hello, "+name+"!!!");	}
+	
 	var name=prompt("what's your name??");
-	hello();	
+	if (name="yar") hell();
+	else hello();	
 	//var mm=confirm("are you fucking serious???")//question
+	var x="lol"
 		
 	for (i=1; i<=5; i++){
 		document.write(x+"_"+i+"<br/>")
